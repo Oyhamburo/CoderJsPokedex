66adc91e1ddf4d02dc4081fa3769855e54f3b30f
Jeremias Aitor <oyhamburo.jeremias@gmail.com>
2022-05-24 19:06:30 -0300

    desafio

--- js/main.js
+++ js/main.js
@@ -42,5 +42,38 @@ search_pokemonName.addEventListener("input", () => {
 
 
 
+// Desafio ARRAY
+function pokemon(nombre, id, tipo){
+    this.nombre = nombre;
+    this.id = id;
+    this.tipo = tipo;
+}
+const pokemon01 = new pokemon ('bulbasaur', 1, 'planta');
+const pokemon02 = new pokemon ('ybasaur', 2, 'planta');
+const pokemon03 = new pokemon ('venasaur', 3, 'planta');
+const pokemon04 = new pokemon ('charmander', 4, 'fuego');
+const pokemon05 = new pokemon ('charmeleon', 5, 'fuego');
+const pokemon06 = new pokemon ('charizard', 6, 'fuego');
+const pokemon07 = new pokemon ('squirtle', 7, 'agua');
+const pokemon08 = new pokemon ('wartortle', 8, 'agua');
+const pokemon09 = new pokemon ('blastoise', 9, 'agua');
+
+
+const arrayPokemon = [ pokemon01, pokemon02, pokemon03, pokemon04, pokemon05, pokemon06, pokemon07, pokemon08, pokemon09];
+
+
+let busqueda = prompt("Pokedex \n Ingrese el id \n (disponible del 1 al 9)");
+
+
+for (let i=0; i<=busqueda; i++){
+    suma = busqueda - 1;
+    if(i === suma){
+        alert("nombre: "+arrayPokemon[i].nombre);
+        alert("tipo: "+arrayPokemon[i].tipo);
+    }
+}
+
+
+
 
 
