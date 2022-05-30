e450f54b35e1c42a0d57aa885befbfc46ec0f783
Jeremias Aitor <oyhamburo.jeremias@gmail.com>
2022-05-30 14:13:13 -0300

    segunda entrega

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function fetchPokemon(id) {
 function createPokemon(pokemon){
     //item 4
     const id = document.querySelector('.item4-div');
-    id.textContent = ('ID: ')+(pokemon.id);
+    id.innerHTML = `ID: ${pokemon.id}`;
     item4js.appendChild(id);
     //item 3
     const imagen = document.querySelector('.pokedex__pantalla__img');
@@ -23,10 +23,11 @@ function createPokemon(pokemon){
     item3js.appendChild(imagen);
     //item2
     const tipo1 = document.querySelector('.tipo-1');
-    tipo1.textContent = pokemon.types[0].type.name;
+    tipo1.innerHTML = pokemon.types[0].type.name;
     item2js.appendChild(tipo1);
     const tipo2 = document.querySelector('.tipo-2');
-    tipo2.textContent = pokemon.types[1].type.name;
+    tipo2.innerHTML = "---";
+    tipo2.innerHTML = pokemon.types[1].type.name;
     item2js.appendChild(tipo2);
     //item 1
     const name = document.querySelector('.item1-div');
@@ -36,43 +37,10 @@ function createPokemon(pokemon){
 
 }
 fetchPokemon(1);
+
 search_pokemonName.addEventListener("input", () => {
     fetchPokemon(search_pokemonName.value.toLowerCase());
   });
-
-
-
-// Desafio ARRAY
-function pokemon(nombre, id, tipo){
-    this.nombre = nombre;
-    this.id = id;
-    this.tipo = tipo;
-}
-const pokemon01 = new pokemon ('bulbasaur', 1, 'planta');
-const pokemon02 = new pokemon ('ybasaur', 2, 'planta');
-const pokemon03 = new pokemon ('venasaur', 3, 'planta');
-const pokemon04 = new pokemon ('charmander', 4, 'fuego');
-const pokemon05 = new pokemon ('charmeleon', 5, 'fuego');
-const pokemon06 = new pokemon ('charizard', 6, 'fuego');
-const pokemon07 = new pokemon ('squirtle', 7, 'agua');
-const pokemon08 = new pokemon ('wartortle', 8, 'agua');
-const pokemon09 = new pokemon ('blastoise', 9, 'agua');
-
-
-const arrayPokemon = [ pokemon01, pokemon02, pokemon03, pokemon04, pokemon05, pokemon06, pokemon07, pokemon08, pokemon09];
-
-
-let busqueda = prompt("Pokedex \n Ingrese el id \n (disponible del 1 al 9)");
-
-
-for (let i=0; i<=busqueda; i++){
-    suma = busqueda - 1;
-    if(i === suma){
-        alert("nombre: "+arrayPokemon[i].nombre);
-        alert("tipo: "+arrayPokemon[i].tipo);
-    }
-}
-
 
 
 
